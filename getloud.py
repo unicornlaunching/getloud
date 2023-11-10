d2f2b2c034f9f0640ea22f755d94bdd395f04b76
unicornlaunching <45397093+unicornlaunching@users.noreply.github.com>
2023-11-09 22:46:40 -0500

    Update getloud.py

--- getloud.py
+++ getloud.py
@@ -1,39 +1,41 @@
 import streamlit as st
-import librosa
-import numpy as np
-from scipy.io import wavfile
-import io
-import matplotlib.pyplot as plt
-from collections import defaultdict
-from tempfile import TemporaryFile
+from pydub import AudioSegment
+from tempfile import NamedTemporaryFile
 
-def get_loudest_timestamps(file):
-    # Load the file as a numpy array with librosa
-    data_wav, sr = librosa.load(file, sr=None)
+def find_loudest_timestamps(audio, num_timestamps):
+    loudest_timestamps = []
+    for _ in range(num_timestamps):
+        loudest_position = audio.dBFS
+        loudest_time = 0
+        chunk_duration = 1000  # 1 second
+        for start in range(0, len(audio), chunk_duration):
+            chunk = audio[start:start + chunk_duration]
+            if chunk.dBFS > loudest_position:
+                loudest_position = chunk.dBFS
+                loudest_time = start / 1000  # Convert milliseconds to seconds
 
-    # Calculate amplitude
-    amplitude = np.abs(librosa.stft(data_wav))
+        loudest_timestamps.append(loudest_time)
 
-    # Find loudest timestamps
-    n_loudest = 20
-    duration = data_wav.shape[0] / sr
-    timestamps = np.linspace(0, duration, amplitude.shape[1])
-    loudest_points = np.argpartition(amplitude.mean(axis=0), -n_loudest)[-n_loudest:]
-    loudest_timestamps = timestamps[loudest_points]
+        # Remove the loudest part to avoid selecting it again
+        audio = audio[:int(loudest_time * 1000)] + audio[int((loudest_time + 1) * 1000):]
 
     return loudest_timestamps
 
 def main():
-    st.title('Audio Analysis with Streamlit')
-    uploaded_file = st.file_uploader("Upload an audio file", type=['mp3'])
+    st.title("Audio Analyzer")
+    audio_file = st.file_uploader("Upload your mp3", type=['mp3'])
+    num_timestamps = st.number_input("Number of timestamps to find", min_value=1, value=10, step=1)
+    
+    if audio_file is not None and num_timestamps is not None:
+        tfile = NamedTemporaryFile(delete=False) 
+        tfile.write(audio_file.read())
+        audio = AudioSegment.from_mp3(tfile.name)
 
-    if uploaded_file is not None:
-        audio_file = TemporaryFile()
-        audio_file.write(uploaded_file.read())
-        audio_file.seek(0)  # Set position to the beginning of the file
+        # Find the loudest timestamps
+        loudest_timestamps = find_loudest_timestamps(audio, num_timestamps)
 
-        loudest_timestamps = get_loudest_timestamps(audio_file)
-        st.write('The loudest timestamps are: ', loudest_timestamps)
-
+        # Print the timestamps
+        st.write(f"Top {num_timestamps} Loudest Timestamps (in seconds):", loudest_timestamps)
+            
 if __name__ == "__main__":
     main()
